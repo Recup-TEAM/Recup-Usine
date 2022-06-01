ac99afbc24606044fe1a195f93eb4ead42547485
Weyrd <wyrd.reisa@gmail.com>
2022-06-01 17:38:58 +0200

    ajout features connexion et hashage de password

--- front/js/login/sign_in.js
+++ front/js/login/sign_in.js
@@ -3,14 +3,35 @@
     return md5(password);
 }
 
+//function get email and password and send it to the server
+function sign_in() {
+    var login = $("#email").val();
+    var password = $("#password").val();
+    logger.sign_in(login, password);
+    //logger.sign_in(login, hashPassword(password));
+}
+
 
 
 $(document).ready(function() {
+
     $(document).on('click', '#connectionButton', function() {
-        var login = $('#email').val();
-        var password = $('#password').val();
-        logger.sign_in(login, password);
+        sign_in();
     });
 
+    // when enter is pressed on the password field
+    $(document).on('keypress', '#password', function(e) {
+        if (e.which == 13) {
+            sign_in();
+        }
+    });
+    
+    // when enter is pressed on the email field
+    $(document).on('keypress', '#email', function(e) {
+        if (e.which == 13) {
+            // focus on the password field
+            $('#password').focus();
+        }
+    });
 
 });
