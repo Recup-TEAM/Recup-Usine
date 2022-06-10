e5a4c493d4e2763646d11f6674bfa32241a4a1d4
Corow <90448778+CorentinDenoulet@users.noreply.github.com>
2022-06-10 10:58:54 +0200

    Itinéraire finis

--- back/Itinary-ask/Itinary-ask.js
+++ back/Itinary-ask/Itinary-ask.js
@@ -1,6 +1,18 @@
 ﻿
 //set map options
-var Myposition = { lat: 50.6341809, lng: 3.0487116 };
+var Myposition = new google.maps.LatLng(50.6341809, 3.0487116);
+var Stop1 = new google.maps.LatLng(50.63409, 3.04116);
+var Stop2 = new google.maps.LatLng(50.6341809, 3.0387116);
+var Stop3 = new google.maps.LatLng(50.63809, 3.067116);
+var Stop4 = "20 rue claude Debussy baisieux";
+var listWaypoint = [Stop1, Stop2, Stop3, Stop4];
+var objectWaypoint = [];
+for (let index = 0; index < listWaypoint.length; index++) {
+    const element = listWaypoint[index];
+    console.log(element);
+    objectWaypoint.push({"location":element, "stopover":true})
+    
+}
 var mapOptions = {
     center: Myposition,
     zoom: 14,
@@ -25,19 +37,18 @@ directionsDisplay.setMap(map);
 function calcRoute() {
     //création de la requete
     var request = {
-        origin: document.getElementById("from").value,
-        destination: document.getElementById("to").value,
-        travelMode: google.maps.TravelMode.DRIVING, //Choix du mode de transport (ici voiture)
-        unitSystem: google.maps.UnitSystem.METRIC
+        origin: Myposition,
+        destination: Myposition,
+        travelMode: google.maps.TravelMode.DRIVING, 
+        unitSystem:  google.maps.UnitSystem.METRIC,
+        waypoints: objectWaypoint,
+        optimizeWaypoints: true,
+        provideRouteAlternatives: false,      
     }
 
     //pass the request to the route method
     directionsService.route(request, function (result, status) {
         if (status == google.maps.DirectionsStatus.OK) {
-
-            //Get distance and time
-            const output = document.querySelector('#output');
-            output.innerHTML = "<div class='alert-info'>De : " + document.getElementById("from").value + ".<br />A : " + document.getElementById("to").value + ".<br /> Distance <i class='fas fa-road'></i> : " + result.routes[0].legs[0].distance.text + ".<br />Durée <i class='fas fa-hourglass-start'></i> : " + result.routes[0].legs[0].duration.text + ".</div>";
 
             //display route
             directionsDisplay.setDirections(result);
@@ -47,22 +58,11 @@ function calcRoute() {
             //center map in London
             map.setCenter(Myposition);
 
-            //show error message
-            output.innerHTML = "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> Could not retrieve driving distance.</div>";
         }
     });
 
 }
 
-
-
-//create autocomplete objects for all inputs
-var options = {
-    types: ['(cities)']
-}
-
-var input1 = document.getElementById("from");
-var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
-
-var input2 = document.getElementById("to");
-var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
+document.addEventListener("DOMContentLoaded", function() {
+    calcRoute();
+  });
