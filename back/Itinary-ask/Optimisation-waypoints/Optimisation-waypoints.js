55f997db6f44db2c91ea65edd525f4c3fe9c5b84
Corow <90448778+CorentinDenoulet@users.noreply.github.com>
2022-06-16 17:29:21 +0200

    Update Optimisation-waypoints.js

--- back/Itinary-ask/Optimisation-waypoints/Optimisation-waypoints.js
+++ back/Itinary-ask/Optimisation-waypoints/Optimisation-waypoints.js
@@ -10,8 +10,10 @@ var waypoint2 = "Lille Grand palais, Lille";
 var waypoint3 = "Zoo de Lille, Lille";
 var waypoint4 = "267 Rue de l'echoppette, 62400, Locon";
 var waypoint5 = "Gare Lille Flandres, Lille";
+var waypoint6 = "297 Chemin des Petits Mas, 13420, Gemenos";
+var waypoint7 = "40 Rue Victor Hugo, Paris"
 
-var listWaypoints = [waypoint1, waypoint2, waypoint3, waypoint4, waypoint5];
+var listWaypoints = [waypoint1, waypoint2, waypoint3, waypoint4, waypoint5, waypoint6, waypoint7];
 
 //javascript.js -------------------------------------------------------------------------------------------------------------------------
 
@@ -25,13 +27,10 @@ var mapOptions = {
 
 //create map
 var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
-
 //create a DirectionsService object to use the route method and get a result for our request
 var directionsService = new google.maps.DirectionsService();
-
 //create a DirectionsRenderer object which we will use to display the route
 var directionsDisplay = new google.maps.DirectionsRenderer();
-
 //bind the DirectionsRenderer to the map
 directionsDisplay.setMap(map);
 
@@ -69,61 +68,66 @@ async function calcRoute(point1, point2) {
             //show error message
             output.innerHTML = "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> Could not retrieve driving distance.</div>";
         }
-        console.log(result);
         data = result.routes[0].legs[0].distance.value;
-        console.log("1",data);
         return data
     });
     //console.log(data);
     return data
 }
-
-
-//create autocomplete objects for all inputs
-var options = {
-    types: ['(cities)']
+async function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-var input1 = document.getElementById("from");
-var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
-
-var input2 = document.getElementById("to");
-var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
 
 // --------------------------------------------------------------------------------------------------------------------------------------
 // TRI DE LA LISTE DE WAYPOINT POUR UN TRAJET OPTIMAL
+async function triListeWaypoint(listWaypoints) {
+    // ListWaypoint = [A, B, C, D, E];   (10 waypoints max)
+    let orderedList = [];                       // liste ordonée
+    let position = myPosition;
+    let alreadyUsed = []
+    var listtall = listWaypoints.length;
 
-// ListWaypoint = [A, B, C, D, E];   (10 waypoints max)
-let orderedList = [];                       // liste ordonée
-let position = origin;
+    for (let i = 0; i < listtall; i++) {
 
+        if (listWaypoints[i]){temp = listWaypoints[i]};
+        for (let j = 0; j < (listWaypoints.length - 1); j++) {
+                let dist1,dist2;
 
-for (let i = 0; i < listWaypoints.length; i++) {
+                console.log("pos1 : ", position, "\ntemp1 : ", temp, "\ndist1 : ", dist1, 
+                "\npos : ", position, "\nlisteWJ+1 : ", listWaypoints[j+1],"\ndist2 : ", dist2);
 
-    let temp = listWaypoints[i];                // variable de stockage temporaire
-    for (let j = 0; j < (listWaypoints.length - 1); j++) {
-        let dist1;
-        let dist2;
-        console.log("pos : ", position, "\ntemp : ", temp);
-        calcRoute(position, temp).then(distopieleretour => {dist1 = distopieleretour;console.log(dist1);});
-        calcRoute(position, listWaypoints[j+1]).then(distopieleretour => {dist2 = distopieleretour});
-        console.log("dist1 : ", dist1, "\ndist2 : ", dist2);
+                if(position != temp) {dist1 = await calcRoute(position, temp)} else {dist1 = 0}
+                if(position != listWaypoints[j+1]){dist2 = await calcRoute(position, listWaypoints[j+1])} else {dist2 = 0}
 
-        if (dist1 > dist2){
-            temp = listWaypoints[j+1];
+                console.log("pos : ", position, "\ntemp : ", temp, "\ndist1 : ", dist1, 
+                "\npos : ", position, "\nlisteWJ+1 : ", listWaypoints[j+1],"\ndist2 : ", dist2);
+
+                console.log(alreadyUsed, !alreadyUsed.includes(temp));
+                if (dist1 > dist2) {
+                    
+                    if (listWaypoints[j+1]){temp = listWaypoints[j+1]};
+                    alreadyUsed.push(temp);
+                    console.log("temp : ", temp, "\nalreadyUsed : ", alreadyUsed);
+                }
+                await timeout(1500);
+                
+                
         }
+        orderedList.push(temp);
+        position = orderedList[i];
+        let indexOf = listWaypoints.indexOf(temp);
+        listWaypoints.splice(indexOf, 1); //console.log au cas où | remove le point sur lequel on est passé
     }
-    orderedList.push(temp);
-    listWaypoints.splice(listWaypoints.indexOf(temp), 1); //console.log au cas où | remove le point sur lequel on est passé
-    position = orderedList[i];
+    console.log("orderedList : ", orderedList, "\nWaypointlist : ", listWaypoints);
+    return orderedList;
 }
-
 //-------------------------------------------------------------------------------------------------------------------------------------
 
 //Vérification de l'affichage du HTML puis lancement du calcul de l'itinéraire
 document.addEventListener("DOMContentLoaded", function() {
-    let dist;
-    calcRoute(myPosition,waypoint1).then(distopie => {dist = distopie});
-    console.log(a);
-    console.log(orderedList);
-  });
+    //call triListeWaypoint function
+    triListeWaypoint(listWaypoints).then(function(result) {
+        console.log("result : ", result);
+    })
+               
+});
