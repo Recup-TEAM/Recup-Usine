60a99bc5bf27b06e5289dd4c3467d8e4b19191b1
Corow <90448778+CorentinDenoulet@users.noreply.github.com>
2022-06-16 10:47:23 +0200

    Update Optimisation-waypoints.js

--- back/Itinary-ask/Optimisation-waypoints/Optimisation-waypoints.js
+++ back/Itinary-ask/Optimisation-waypoints/Optimisation-waypoints.js
@@ -17,7 +17,6 @@ var mapOptions = {
     center: { lat: 50.634742, lng: 3.048682 },
     zoom: 14,
     mapTypeId: google.maps.MapTypeId.ROADMAP
-
 };
 
 //create map
@@ -26,46 +25,33 @@ var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
 //create a DirectionsService object to use the route method and get a result for our request
 var directionsService = new google.maps.DirectionsService();
 
-//create a DirectionsRenderer object which we will use to display the route
-var directionsDisplay = new google.maps.DirectionsRenderer();
+// -----------------------------------------------------------------------------------------------------------------------------------------
+//FONCTION DE CALCUL DE LA DISTANCE ENTRE DEUX POINTS
 
-//bind the DirectionsRenderer to the map
-directionsDisplay.setMap(map);
+function calcDist(origin, destination) {
 
-
-//define calcRoute function
-function calcRoute() {
-    //create request
+    // Création de la requete
     let request = {
-        origin: myPosition,
-        destination: waypoint4,
+        origin: origin,
+        destination: destination,
         travelMode: google.maps.TravelMode.DRIVING,
         unitSystem: google.maps.UnitSystem.METRIC
     }
 
-    //pass the request to the route method
+    // Calcul du trajet le plus court possible
     directionsService.route(request, function (result, status) {
         if (status == google.maps.DirectionsStatus.OK) {
 
-            //Get distance and time
+            //Récupération de la distance
             const output = document.querySelector('#output');
             output.innerHTML = "<div class='alert-info'>Driving distance <i class='fas fa-road'></i> : " + result.routes[0].legs[0].distance.value + ".</div>";
 
-            //display route
-            directionsDisplay.setDirections(result);
         }
     });
-}
-// -------------------------------------------------------------------------------------------------------------------------------------
-
-// définiton de la fontion de calcule de distance
-function calcDist(origin, destination){
-    //call calcRoute()
-
+    return result.routes[0].legs[0].distance.value
 }
 
 // --------------------------------------------------------------------------------------------------------------------------------------
-
 // TRI DE LA LISTE DE WAYPOINT POUR UN TRAJET OPTIMAL
 
 // ListWaypoint = [A, B, C, D, E];   (10 waypoints max)
@@ -91,5 +77,5 @@ for (let i = 0; i < listWaypoints.length; i++) {
 
 //Vérification de l'affichage du HTML puis lancement du calcul de l'itinéraire
 document.addEventListener("DOMContentLoaded", function() {
-    calcRoute();
+    calcDist();
   });
