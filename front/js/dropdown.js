d9a49f863e28d7309307f98f6a0b379287428a80
Armand <armand.deffrennes@student.yncrea.fr>
2022-06-20 09:20:30 +0200

    Collector Admin HTML Dropdown menu + dropdown JS FRONT completed

--- front/js/dropdown.js
+++ front/js/dropdown.js
@@ -127,7 +127,51 @@ $(document).ready(function () {
                             }
                         }
                         else {
-                            $("#profileInteraction").html(`<div class="btn-group" id="dropdownProfile">
+                            let currentPage = window.location.href.split('/').splice(window.location.href.split('/').length - 1, window.location.href.split('/').length);
+                            if (currentPage[0] === "collectorManagement" || currentPage[0] === "collectorCreationManagement" || currentPage[0] === "specificCollectorManagement"){
+                                $("#profileInteraction").html(`<div class="btn-group" id="dropdownProfile">
+                <button class="btn btn-connection calibri-regular-normal-white-22px hvr-sweep-to-right dropdown-toggle" type="button" data-toggle="dropdown">
+                    ${name}
+                </button>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item hvr-sweep-to-right" href="/recupartenaireHtml/partnerProfile"><span class="fa fa-user"></span> Profile</a>
+                    <a class="dropdown-item hvr-sweep-to-right" href="/adminHtml/recuplateformeAdminHtml/recuplateformeManagement"><span class="fa fa-globe"></span> Recuplateforme</a>
+                    <a class="dropdown-item hvr-sweep-to-right" href="/adminHtml/recupartenaireAdminHtml/partnerManagement"><span class="fa fa-handshake-o"></span> Recupartenaire</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item hvr-sweep-to-right" href="#" id="logoutButton"><span class="fa fa-sign-out"></span> Déconnexion</a>
+                </div>
+            </div>`);
+                            }
+                            else if (currentPage[0] === "partnerDemandsManagement" || currentPage[0] === "partnerManagement" || currentPage[0] === "specificPartnerManagement" || currentPage[0] === "specificPartnerModificationManagement"){
+                                $("#profileInteraction").html(`<div class="btn-group" id="dropdownProfile">
+                <button class="btn btn-connection calibri-regular-normal-white-22px hvr-sweep-to-right dropdown-toggle" type="button" data-toggle="dropdown">
+                    ${name}
+                </button>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item hvr-sweep-to-right" href="/recupartenaireHtml/partnerProfile"><span class="fa fa-user"></span> Profile</a>
+                    <a class="dropdown-item hvr-sweep-to-right" href="/adminHtml/recuplateformeAdminHtml/recuplateformeManagement"><span class="fa fa-globe"></span> Recuplateforme</a>
+                    <a class="dropdown-item hvr-sweep-to-right" href="/adminHtml/collectorAdminHtml/collectorManagement"><span class="fa fa-truck"></span> Collecteur</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item hvr-sweep-to-right" href="#" id="logoutButton"><span class="fa fa-sign-out"></span> Déconnexion</a>
+                </div>
+            </div>`);
+                            }
+                            else if (currentPage[0] === "recuplateformeManagement" || currentPage[0] === "specificRecuplateformeManagement"){
+                                $("#profileInteraction").html(`<div class="btn-group" id="dropdownProfile">
+                <button class="btn btn-connection calibri-regular-normal-white-22px hvr-sweep-to-right dropdown-toggle" type="button" data-toggle="dropdown">
+                    ${name}
+                </button>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item hvr-sweep-to-right" href="/recupartenaireHtml/partnerProfile"><span class="fa fa-user"></span> Profile</a>
+                    <a class="dropdown-item hvr-sweep-to-right" href="/adminHtml/collectorAdminHtml/collectorManagement"><span class="fa fa-truck"></span> Collecteur</a>
+                    <a class="dropdown-item hvr-sweep-to-right" href="/adminHtml/recupartenaireAdminHtml/partnerManagement"><span class="fa fa-handshake-o"></span> Recupartenaire</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item hvr-sweep-to-right" href="#" id="logoutButton"><span class="fa fa-sign-out"></span> Déconnexion</a>
+                </div>
+            </div>`);
+                            }
+                            else if (currentPage[0] === ""){
+                                $("#profileInteraction").html(`<div class="btn-group" id="dropdownProfile">
                 <button class="btn btn-connection calibri-regular-normal-white-22px hvr-sweep-to-right dropdown-toggle" type="button" data-toggle="dropdown">
                     ${name}
                 </button>
@@ -140,6 +184,7 @@ $(document).ready(function () {
                     <a class="dropdown-item hvr-sweep-to-right" href="#" id="logoutButton"><span class="fa fa-sign-out"></span> Déconnexion</a>
                 </div>
             </div>`);
+                            }
                         }
                         //event listener on click on buttonSignOut
                         $("#logoutButton").click(function (){
