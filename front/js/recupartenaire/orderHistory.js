f83c19eeee2b5fd3cd8d4119b849220fc1019ddb
Weyrd <44951922+Weyrd@users.noreply.github.com>
2022-06-20 17:05:24 +0200

    jsp

--- front/js/recupartenaire/orderHistory.js
+++ front/js/recupartenaire/orderHistory.js
@@ -1,7 +1,35 @@
-
+var picker
+function getInterval(){
+    var a = picker.getStartDate();
+    var b = picker.getEndDate();
+    console.log("date star", a, "/ndate end", b);
+}
 
 //when the page is loaded
 $(document).ready(function () {
-    console.log("document ready");
+    $("#datepicker").daterangepicker();
+    picker = new easepick.create({
+        element: "#datepicker",
+        css: [
+            "https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.0/dist/index.css"
+        ],
+        zIndex: 10,
+        lang: "fr-FR",
+        format: "DD MMM YYYY",
+        plugins: [
+            "RangePlugin"
+        ]
+    })
+
+    picker.on("rangeChange", getInterval());
+
+    // id_entreprise = 1
+    // dataEntreprise = api_request.getAllEntrepriseOfUser(id_entreprise)
+    // console.log("dataEntreprise", dataEntreprise);
+
+    // allProducts = api_request.getProductsByEntrepriseId(id_entreprise)
+    // console.log("allProducts", allProducts);
+    
+    
 });
 
