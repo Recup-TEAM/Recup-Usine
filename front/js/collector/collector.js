0c7a04e09680397dc63678c511c3ea92897524f5
Armand <armand.deffrennes@student.yncrea.fr>
2022-06-23 11:02:10 +0200

    collector Beginning

--- front/js/collector/collector.js
+++ front/js/collector/collector.js
@@ -1,18 +1,27 @@
 $(document).ready(function () {
     // ajax request to "/api/user/connected" to know if user if connected
     $.ajax({
-        url: "/api/user/get/connected",
+        url: "/api/user/get/data",
         type: "GET",
         success: (data) => {
-            let divConnexion;
-            if (!data.connected) {
+            if (!data.success) {
                 connected = false;
                 if (window.location.href.split('/').length !== 4) {
                     window.location.href = '/'
                 }
             } else {
-                let name = data.email.split("@")[0];
+                let name = data.data.email.split("@")[0];
                 document.getElementById("collectorName").textContent = name;
+
+                //Collector Infos
+                $.ajax({
+                    url: "/api/collector/get/collectorByIdUser/" + data.data.id,
+                    type: "GET",
+                    success: (dataCollector) => {
+                        document.getElementById('vehicule').textContent = "Camion " + dataCollector.data[0].id;
+                        document.getElementById('nbCollect').textContent = dataCollector.data[0].tour;
+                    }
+                });
             }
         }
     });
